b5bbe063ba4455f1133a731e1ca4255ec7d0ec02
Jacob-A-Cox <jacob.a.cox21@gmail.com>
2024-05-04 09:30:46 -0700

    test 2 for final

--- src/gencontent.py
+++ src/gencontent.py
@@ -30,12 +30,12 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
     for filename in os.listdir(dir_path_content):
         from_path = os.path.join(dir_path_content, filename)
         dest_path = os.path.join(dest_dir_path, filename)
-        if not os.path.isfile(from_path):
-            generate_pages_recursive(from_path, template_path, dest_path)
-        elif os.path.isfile(from_path) and not from_path.endswith("md"):
-            continue
-        elif os.path.isfile(from_path) and from_path.endswith("md"):
+        if os.path.isfile(from_path):
+            if not from_path.endswith("md"):
+                continue
             generate_page(from_path, template_path, dest_path)
+        elif not os.path.isfile(from_path):
+            generate_pages_recursive(from_path, template_path, dest_path)
 
 
 def extract_title(md):
